9ddfe08244c5bd5aef9b0b3b24733a8d18e88e88
Martin J. Pollard <martin0566@gmail.com>
2016-03-03 16:08:15 -0500

    Install SuperSU when flashing ROM

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -116,6 +116,11 @@ class EdifyGenerator(object):
     self.script.append('set_perm(0, 0, 0777, "/tmp/verify_cache_partition_size.sh");')
     self.script.append('run_program("/tmp/verify_cache_partition_size.sh");')
 
+  def FlashSuperSU(self):
+    self.script.append('package_extract_dir("supersu", "/tmp/supersu");')
+    self.script.append('run_program("/sbin/busybox", "unzip", "/tmp/supersu/supersu.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");')
+    self.script.append('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/supersu.zip");')
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
